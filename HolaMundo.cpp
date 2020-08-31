037b6ebad88fec03c9a355e5e2238bb86c2a8b32
Freddy875 <fer875nandosm@gmail.com>
2020-08-31 18:48:45 -0500

    Estos son los tipos de datos basicos en C++

---
+++ HolaMundo.cpp
@@ -0,0 +1,26 @@
+//Primer programa en C++
+
+#include<iostream>
+/*
+Esto es una libreria 
+iostream significa Input/Output
+Entrada y Salida de datos
+*/
+
+using namespace std;
+
+int main(){
+	
+	//std::cout<<
+	/*Se tiene que poner así cuando no se pone el using namespace std*/
+	
+	cout<<"Hola Mundo"<<endl;
+	
+	cout<<"Hola otra vez mundo\n";
+	/*
+	cout Imprime en salida de pantalla todo lo que esta adentro de las comillas 
+	endl es un salto de linea
+	*/
+	return 0;
+	
+}// fin int main
---
+++ TiposDatosBasicos.cpp
@@ -0,0 +1,27 @@
+//Tipos de datos básicos en C++
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int iNumero = 1;
+	
+	float fReal = 10.45;
+	
+	double dDoble = 16.3546;
+	
+	char cLetra = 'A';
+	
+	cout<<iNumero<<endl;
+	
+	cout<<fReal<<endl;
+	
+	cout<<dDoble<<endl;
+	
+	cout<<cLetra<<endl;
+	
+	return 0;
+	
+}//fin int main
