cc8fa10888a0d47878930789ae3fa3bdddd1e49f
Freddy875 <fer875nandosm@gmail.com>
2020-08-31 19:24:18 -0500

    Aprnedimos como leer datos y como poner acentos

---
+++ EntradaDeDatos.cpp
@@ -0,0 +1,26 @@
+//Lectura o entrada de datos 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int iNumero1; //Define la variable 
+	
+	cout<<"Ingresa un n\xA3mero (entero): ";
+	
+	cin>>iNumero1; //Guardando la variable 
+	
+	cout<<"\nEl n\xA3mero que ingresaste es: "<<iNumero1<<endl;
+
+	float fNumero1;
+	
+	cout<<"Ingresa otro n\xA3mero (real): ";
+	
+	cin>>fNumero1;
+	
+	cout<<"\nEl n\xA3mero que ingresaste es: "<<fNumero1;
+	
+	return 0;
+}//fin int main
---
+++ Machote.cpp
@@ -0,0 +1,11 @@
+//Machote
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	
+	return 0;
+}//fin int main
